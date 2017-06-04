5314d2a318ef2af15647e6637b3b4478ff506219
soarflat <soarflat777@gmail.com>
2017-06-04 23:16:02 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    app: './src/app.js',
+  },
+
+  output: {
+    path: path.join(__dirname, 'docs'),
+    filename: '[name].bundle.js',
+  },
+
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: true,
+          presets: ['env'],
+        },
+      }],
+    }],
+  },
+};
